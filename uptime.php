0250298ce378ec029f37062920d5ceffcdce1185
JanusLin <j4nusl1n@gmail.com>
2015-06-09 12:56:02 +0800

    Fix issues about output time of uptime.php

--- uptime.php
+++ uptime.php
@@ -1,4 +1,6 @@
 <?php
+	header("Content-Type:text/html; charset=utf-8");
+	date_default_timezone_set("Asia/Taipei");
 	function getTime($name){
 		$content=file_get_contents("http://nightdev.com/hosted/uptime.php?channel=$name");
 		//echo $content;
@@ -6,11 +8,19 @@
 		else{
 			//echo $content;
 			$aryStr=explode(" ", $content);
+			//echo count($aryStr);
 			//echo $aryStr[0]." ".$aryStr[2];
-			date_default_timezone_set("Asia/Taipei");
-			$h=date("H");
-			$m=date("i");
-			echo "開台時間: ".date("m/d H:i", mktime($h-$aryStr[0], $m-$aryStr[2]))."。開台時數: ".$aryStr[0]." 小時 ".$aryStr[2]." 分";
+			$hour=date("H");
+			$minute=date("i");
+			if(count($aryStr)>2){
+				echo "開台時間: ".date("m/d H:i", mktime($hour-$aryStr[0], $minute-$aryStr[2]))."。開台時數: ".$aryStr[0]." 小時 ".$aryStr[2]." 分";
+			}
+			else{
+				if($aryStr[1]=="hours"||$aryStr[1]=="hour")
+					echo "開台時間: ".date("m/d H:i", mktime($hour-$aryStr[0], $minute))."。開台時數: ".$aryStr[0]." 小時";
+				else if($aryStr[1]=="minutes"||$aryStr[1]=="minutes")
+					echo "開台時間: ".date("m/d H:i", mktime($hour, $minute-$aryStr[0]))."。開台時數: ".$aryStr[0]." 分";
+			}
 		}
 	}
 	getTime($_GET["name"]);
